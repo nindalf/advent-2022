0b2b02d353ddea058a57334f7c144c70d1c5b78e
Krisna Sundarram <krishnasundarram@monzo.com>
2022-12-01 09:26:46 +0000

    Small readability cleanup on day 1

--- src/day01.rs
+++ src/day01.rs
@@ -13,13 +13,13 @@ fn max_calories_3(input: String) -> i32 {
 }
 
 fn heap_of_calories(input: String) -> BinaryHeap<i32> {
-    input //                                              "1000\n2000\n3000\n\n4000\n\n5000\n6000\n"
-        .lines() //                                       ["1000", "2000", "3000", "", "4000", "", "5000", "6000"]
-        .map(|line| line.trim().parse::<i32>()) //        [Ok(1000), Ok(2000), Ok(3000), ParseIntError, Ok(4000), ParseIntError, Ok(5000), Ok(6000)]
+    input //                                                            "1\n2\n3\n\n4\n\n5\n6\n"
+        .lines() //                                                     ["1", "2", "3", "", "4", "", "5", "6"]
+        .map(|line| line.trim().parse::<i32>()) //                      [Ok(1), Ok(2), Ok(3), ParseIntError, Ok(4), ParseIntError, Ok(5), Ok(6)]
         .collect::<Vec<_>>()
-        .split(|num| num.is_err()) //                     [[Ok(1000), Ok(2000), Ok(3000)], [Ok(4000)], [Ok(5000), Ok(6000)]]
-        .map(|arr| arr.iter().filter_map(|i| i.as_ref().ok()).sum()) // [6000, 4000, 11000]
-        .collect() //                                      BinaryHeap {data: [4000, 6000, 11000]}
+        .split(|num| num.is_err()) //                                   [[Ok(1), Ok(2), Ok(3)], [Ok(4)], [Ok(5), Ok(6)]]
+        .map(|arr| arr.iter().filter_map(|i| i.as_ref().ok()).sum()) // [6, 4, 11]
+        .collect() //                                                   BinaryHeap {data: [4, 6, 11]}
 }
 
 #[cfg(test)]
@@ -28,22 +28,22 @@ mod tests {
 
     #[test]
     fn part_1_test() -> Result<()> {
-        test("inputs/day1-test.txt", &super::max_calories, 24000)
+        test("inputs/day01-test.txt", &super::max_calories, 24000)
     }
 
     #[test]
     fn part_1_real() -> Result<()> {
-        test("inputs/day1.txt", &super::max_calories, 70698)
+        test("inputs/day01.txt", &super::max_calories, 70698)
     }
 
     #[test]
     fn part_2_test() -> Result<()> {
-        test("inputs/day1-test.txt", &super::max_calories_3, 45000)
+        test("inputs/day01-test.txt", &super::max_calories_3, 45000)
     }
 
     #[test]
     fn part_2_real() -> Result<()> {
-        test("inputs/day1.txt", &super::max_calories_3, 206643)
+        test("inputs/day01.txt", &super::max_calories_3, 206643)
     }
 
     fn test(test_file: &str, function: &dyn Fn(String) -> i32, expected_val: i32) -> Result<()> {
