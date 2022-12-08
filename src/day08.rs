977a34eb7cd933461a22ceb72fc64a043d6a7bab
Krisna Sundarram <krishnasundarram@monzo.com>
2022-12-08 12:31:26 +0000

    Improve perf for day 8 by 80% again

--- src/day08.rs
+++ src/day08.rs
@@ -1,115 +1,74 @@
 use crate::matrix::{Matrix, Point};
+use take_until::TakeUntilExt;
 
 #[inline]
 pub fn part_1(input: &str) -> usize {
     let matrix = Matrix::new(input);
-    let mut trees = vec![Tree::default(); matrix.len()];
 
-    for point in matrix.all_points() {
-        let tree_idx = matrix.idx(&point);
-        for x in point.x+1 .. matrix.max_x {
-            let new_point = Point{x, y: point.y};
-            if matrix.value(&new_point) >= matrix.value(&point) {
-                trees[tree_idx].visible_from_right = false;
-                break
-            }
-        }
+    matrix
+        .all_points()
+        .filter(|point| {
+            let tree_val = matrix.value(point);
 
-        for x in (0 .. point.x).rev() {
-            let new_point = Point{x, y: point.y};
-            if matrix.value(&new_point) >= matrix.value(&point) {
-                trees[tree_idx].visible_from_left = false;
-                break
-            }
-        }
+            let invisible_right = (point.x + 1..matrix.max_x)
+                .map(|x| Point { x, y: point.y })
+                .any(|p| matrix.value(&p) >= tree_val);
 
-        for y in point.y+1 .. matrix.max_y {
-            let new_point = Point{x: point.x, y};
-            if matrix.value(&new_point) >= matrix.value(&point) {
-                trees[tree_idx].visible_from_bottom = false;
-                break
-            }
-        }
+            let invisible_left = (0..point.x)
+                .rev()
+                .map(|x| Point { x, y: point.y })
+                .any(|p| matrix.value(&p) >= tree_val);
 
-        for y in (0 .. point.y).rev() {
-            let new_point = Point{x: point.x, y};
-            if matrix.value(&new_point) >= matrix.value(&point) {
-                trees[tree_idx].visible_from_top = false;
-                break
-            }
-        }
-    }
-    trees.iter().filter(|tree| tree.visible()).count()
+            let invisible_bottom = (point.y + 1..matrix.max_y)
+                .map(|y| Point { x: point.x, y })
+                .any(|p| matrix.value(&p) >= tree_val);
+
+            let invisible_top = (0..point.y)
+                .rev()
+                .map(|y| Point { x: point.x, y })
+                .any(|p| matrix.value(&p) >= tree_val);
+
+            !invisible_right || !invisible_left || !invisible_bottom || !invisible_top
+        })
+        .count()
 }
 
 #[inline]
 pub fn part_2(input: &str) -> usize {
     let matrix = Matrix::new(input);
-    let mut trees = vec![Tree::default(); matrix.len()];
 
-    for point in matrix.all_points() {
-        let tree_idx = matrix.idx(&point);
-        for x in point.x+1 .. matrix.max_x {
-            let new_point = Point{x, y: point.y};
-            trees[tree_idx].trees_right += 1;
-            if matrix.value(&new_point) >= matrix.value(&point) {
-                break
-            }
-        }
+    matrix
+        .all_points()
+        .map(|point| {
+            let tree_val = matrix.value(&point);
 
-        for x in (0 .. point.x).rev() {
-            let new_point = Point{x, y: point.y};
-            trees[tree_idx].trees_left += 1;
-            if matrix.value(&new_point) >= matrix.value(&point) {
-                break
-            }
-        }
+            let trees_right = (point.x + 1..matrix.max_x)
+                .map(|x| Point { x, y: point.y })
+                .take_until(|p| matrix.value(p) >= tree_val)
+                .count();
 
-        for y in point.y+1 .. matrix.max_y {
-            let new_point = Point{x: point.x, y};
-            trees[tree_idx].trees_bottom += 1;
-            if matrix.value(&new_point) >= matrix.value(&point) {
-                break
-            }
-        }
+            let trees_left = (0..point.x)
+                .rev()
+                .map(|x| Point { x, y: point.y })
+                .take_until(|p| matrix.value(p) >= tree_val)
+                .count();
 
-        for y in (0 .. point.y).rev() {
-            let new_point = Point{x: point.x, y};
-            trees[tree_idx].trees_top += 1;
-            if matrix.value(&new_point) >= matrix.value(&point) {
-                break
-            }
-        }
-    }
-    trees.iter().map(|tree| tree.scenic_score()).max().unwrap()
-}
+            let trees_bottom = (point.y + 1..matrix.max_y)
+                .map(|y| Point { x: point.x, y })
+                .take_until(|p| matrix.value(p) >= tree_val)
+                .count();
 
-#[derive(Clone, Debug)]
-struct Tree {
-    visible_from_top: bool,
-    visible_from_left: bool,
-    visible_from_right: bool,
-    visible_from_bottom: bool,
-    trees_top: usize,
-    trees_left: usize,
-    trees_right: usize,
-    trees_bottom: usize,
-}
+            let trees_top = (0..point.y)
+                .rev()
+                .map(|y| Point { x: point.x, y })
+                .take_until(|p| matrix.value(p) >= tree_val)
+                .count();
 
-impl Default for Tree {
-    fn default() -> Self {
-        Self { visible_from_top: true, visible_from_left: true, visible_from_right: true, visible_from_bottom: true, trees_top: 0, trees_left: 0, trees_right: 0, trees_bottom: 0 }
-    }
-}
-
-impl Tree {
-    fn visible(&self) -> bool {
-        self.visible_from_top || self.visible_from_left || self.visible_from_right || self.visible_from_bottom
-    }
-
-    fn scenic_score(&self) -> usize {
-        self.trees_top * self.trees_left * self.trees_right * self.trees_bottom
-    }
+            // scenic score
+            trees_right * trees_left * trees_bottom * trees_top
+        })
+        .max()
+        .unwrap()
 }
 
 #[cfg(test)]
@@ -140,5 +99,4 @@ mod tests {
         let output = super::part_2(FULL_INPUT);
         assert_eq!(output, 268912);
     }
-
 }
