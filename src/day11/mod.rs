b08bdfb05bd8d0c50c0062b052f6b02ba8126f41
Krisna Sundarram <krishnasundarram@monzo.com>
2022-12-11 15:26:32 +0000

    Optimize day 11 to run in 5ms

--- src/day11/mod.rs
+++ src/day11/mod.rs
@@ -1,22 +1,22 @@
 #[inline]
-pub fn part_1(input: &str) -> u128 {
+pub fn part_1(input: &str) -> u64 {
     let monkeys = get_monkeys(input);
-    simulate(monkeys, 20, &|item| item/3)
+    simulate(monkeys, 20, &|item| item / 3)
 }
 
 #[inline]
-pub fn part_2(input: &str) -> u128 {
+pub fn part_2(input: &str) -> u64 {
     let monkeys = get_monkeys(input);
-    let modulo: u128 = monkeys.iter().map(|m| m.divisible_by).product();
+    let modulo: u64 = monkeys.iter().map(|m| m.divisible_by).product();
     simulate(monkeys, 10000, &|item| item % modulo)
 }
 
-fn simulate(mut monkeys: Vec<Monkey>, rounds: u128, worry_limiter: &dyn Fn(u128) -> u128) -> u128 {
+fn simulate(mut monkeys: Vec<Monkey>, rounds: u64, worry_limiter: &dyn Fn(u64) -> u64) -> u64 {
     let mut operations = vec![0; monkeys.len()];
-    for _ in 0 .. rounds {
-        for i in 0 .. monkeys.len() {
+    for _ in 0..rounds {
+        for i in 0..monkeys.len() {
             let m = monkeys[i].clone();
-            monkeys[i].items = Vec::new();
+            monkeys[i].items.clear();
             for item in m.items {
                 let item = match m.operation {
                     Op::Add(arg) => item + arg,
@@ -36,14 +36,18 @@ fn simulate(mut monkeys: Vec<Monkey>, rounds: u128, worry_limiter: &dyn Fn(u128)
     }
     operations.sort();
     let l = operations.len();
-    operations[l-1] * operations[l-2]
+    operations[l - 1] * operations[l - 2]
 }
 
 fn get_monkeys(input: &str) -> Vec<Monkey> {
-    input.split("\n\n")
+    input
+        .split("\n\n")
         .map(|m| {
             let lines: Vec<_> = m.lines().skip(1).map(|l| l.trim()).collect();
-            let items = lines[0][16..].split(", ").map(|x| x.parse().unwrap()).collect();
+            let items = lines[0][16..]
+                .split(", ")
+                .map(|x| x.parse().unwrap())
+                .collect();
             let operation = if lines[1] == "Operation: new = old * old" {
                 Op::Square
             } else if lines[1].starts_with("Operation: new = old * ") {
@@ -52,26 +56,36 @@ fn get_monkeys(input: &str) -> Vec<Monkey> {
                 Op::Add(lines[1]["Operation: new = old + ".len()..].parse().unwrap())
             };
             let divisible_by = lines[2]["Test: divisible by ".len()..].parse().unwrap();
-            let dest_monkey_true = lines[3]["If true: throw to monkey ".len()..].parse().unwrap();
-            let dest_monkey_false = lines[4]["If false: throw to monkey ".len()..].parse().unwrap();
-            Monkey{items, operation, divisible_by, dest_monkey_true, dest_monkey_false}
+            let dest_monkey_true = lines[3]["If true: throw to monkey ".len()..]
+                .parse()
+                .unwrap();
+            let dest_monkey_false = lines[4]["If false: throw to monkey ".len()..]
+                .parse()
+                .unwrap();
+            Monkey {
+                items,
+                operation,
+                divisible_by,
+                dest_monkey_true,
+                dest_monkey_false,
+            }
         })
         .collect()
 }
 
 #[derive(Clone)]
 pub struct Monkey {
-    items: Vec<u128>,
+    items: Vec<u64>,
     operation: Op,
-    divisible_by: u128,
+    divisible_by: u64,
     dest_monkey_true: usize,
     dest_monkey_false: usize,
 }
 
 #[derive(Clone)]
 enum Op {
-    Add(u128),
-    Mul(u128),
+    Add(u64),
+    Mul(u64),
     Square,
 }
 
@@ -88,7 +102,6 @@ mod tests {
 
     #[test]
     fn part_1() {
-
         let output = super::part_1(FULL_INPUT);
         assert_eq!(output, 58786);
     }
@@ -104,5 +117,4 @@ mod tests {
         let output = super::part_2(FULL_INPUT);
         assert_eq!(output, 14952185856);
     }
-
 }
