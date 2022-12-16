6f1483f576fe78e5dec217859cbbb1c5ae12e735
Krisna Sundarram <krishnasundarram@monzo.com>
2022-12-16 07:42:21 +0000

    Partially complete day 15

--- src/day15/mod.rs
+++ src/day15/mod.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashSet};
+use std::collections::HashSet;
 
 use scan_fmt::scan_fmt;
 
@@ -9,7 +9,11 @@ pub fn part_1(input: &str) -> usize {
         .iter()
         .flat_map(|s| [s.nearest_beacon, s.sensor_location])
         .collect::<HashSet<Point>>();
-    let (min_x, max_x, y) = if sensors.len() == 14 {(-2, 25, 10)} else {(-1091140, 4992558, 2000000)};
+    let (min_x, max_x, y) = if sensors.len() == 14 {
+        (-2, 25, 10)
+    } else {
+        (-1091140, 4992558, 2000000)
+    };
     (min_x..=max_x)
         .zip(std::iter::repeat(y))
         .map(|(x, y)| Point { x, y })
@@ -21,23 +25,26 @@ pub fn part_1(input: &str) -> usize {
 #[inline]
 pub fn part_2(input: &str) -> i32 {
     let sensors = get_sensors(input);
-    let all_points = sensors
+    let ans = Point {
+        x: 2843633,
+        y: 2948438,
+    };
+    sensors.iter().for_each(|s| {
+        println!("{} {} {} {}", s.sensor_range, (s.sensor_location.x - ans.x).abs() + (s.sensor_location.y - ans.y).abs(), s.sensor_range < (s.sensor_location.x - ans.x).abs() + (s.sensor_location.y - ans.y).abs(), s.within_range(&ans))
+    });
+    let boundary = if sensors.len() == 14 {
+        20
+    } else {
+        4000000
+    };
+    sensors
         .iter()
-        .flat_map(|s| [s.nearest_beacon, s.sensor_location])
-        .collect::<HashSet<Point>>();
-    
-    let (max_x, max_y) = if sensors.len() == 14 {(20, 20)} else {(4000000, 4000000)};
-    for x in 0 .. max_x {
-        for y in 0 .. max_y {
-            let point = Point {x, y};
-            let outside_sensors = !sensors.iter().any(|s| s.within_range(&point));
-            if !all_points.contains(&point) && outside_sensors {
-                dbg!(x, y);
-                return x * 4000000 + y;
-            }
-        }
-    }
-    0
+        .flat_map(|s| s.manhattan_circle())
+        .filter(|point| point.x >= 0 && point.x <= boundary && point.y >= 0 && point.y <= boundary)
+        .filter(|point| !sensors.iter().any(|s| s.within_range(&point)))
+        .map(|point| point.x * 4000000 + point.y)
+        .next()
+        .unwrap()
 }
 
 fn get_sensors(input: &str) -> Vec<Sensor> {
@@ -91,6 +98,20 @@ impl Sensor {
         let distance =
             (self.sensor_location.x - point.x).abs() + (self.sensor_location.y - point.y).abs();
         distance <= self.sensor_range
+    }
+
+    fn manhattan_circle(&self) -> impl Iterator<Item = Point> {
+        let (x, y) = (self.sensor_location.x, self.sensor_location.y);
+        let top_left = (x - self.sensor_range - 1..=x).zip((y - self.sensor_range - 1..=y).rev());
+        let top_right = (x..=x + self.sensor_range + 1).zip((y..=y + self.sensor_range + 1).rev());
+        let bottom_left = (x - self.sensor_range - 1..=x).zip(y..=y + self.sensor_range + 1);
+        let bottom_right =
+            (x..=x + self.sensor_range + 1).zip((y..=y + self.sensor_range + 1).rev());
+        top_left
+            .chain(top_right)
+            .chain(bottom_left)
+            .chain(bottom_right)
+            .map(|(x, y)| Point { x, y })
     }
 }
 
